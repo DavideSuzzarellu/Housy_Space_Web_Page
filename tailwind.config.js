744ff9e0372fe7cc52cfdd92b175d561cd00767c
DavideSuzzarellu <davide.suzzarellu@hotmail.com>
2024-06-08 13:41:26 +0200

    creacrion archivo

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'pantone-light': 'hsl(178, 100%, 37%)',
+        'pantole-dark': 'hsl(184, 100%, 24%)'
+      },
+      fontFamily: {
+        montserrat : 'Montserrat'
+      }
+    },
+  },
+  plugins: [],
+}
+
