f33bf358674d9e2955be96ddd3449735db7c4d61
DavideSuzzarellu <davide.suzzarellu@hotmail.com>
2024-06-11 20:56:05 +0200

    Creacion header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
+import { Header } from "./components/Header"
+
 function App() {
   return (
-    <>
-      <div>
-      <h1 className="text-pantone-light">Ciao</h1>
-    </div>
-    </>
+      <Header/>
   )
 }
 
